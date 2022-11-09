c173a0c38c5416a161d27128babe9cc8aefa019e
Riccardo De Agostini <me@riccar.do>
2022-11-09 10:27:26 +0100

    Add CompilerFeatureRequiredAttribute

---
+++ src/PolyKit.Polyfills/System/Runtime/CompilerServices/CompilerFeatureRequiredAttribute.cs
@@ -0,0 +1,51 @@
+﻿#if NET7_0_OR_GREATER
+
+#if POLYKIT_PUBLIC
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Runtime.CompilerServices.CompilerFeatureRequiredAttribute))]
+#endif
+
+#else
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Indicates that compiler support for a particular feature is required for the location where this attribute is applied.
+/// </summary>
+[AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+public // polyfill!
+sealed class CompilerFeatureRequiredAttribute : Attribute
+{
+    /// <summary>
+    /// The <see cref="FeatureName"/> used for the ref structs C# feature.
+    /// </summary>
+    public const string RefStructs = nameof(RefStructs);
+
+    /// <summary>
+    /// The <see cref="FeatureName"/> used for the required members C# feature.
+    /// </summary>
+    public const string RequiredMembers = nameof(RequiredMembers);
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CompilerFeatureRequiredAttribute"/> class.
+    /// </summary>
+    /// <param name="featureName">The name of the required compiler feature.</param>
+    public CompilerFeatureRequiredAttribute(string featureName)
+    {
+        FeatureName = featureName;
+    }
+
+    /// <summary>
+    /// Gets the name of the compiler feature.
+    /// </summary>
+    public string FeatureName { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether the compiler can choose to allow access to the location where this attribute is applied
+    /// if it does not understand <see cref="FeatureName"/>.
+    /// </summary>
+    public bool IsOptional { get; init; }
+}
+
+#endif
