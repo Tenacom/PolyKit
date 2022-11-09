07e44610f36bd56fba8b359648ff54d1e50525dc
Riccardo De Agostini <me@riccar.do>
2022-11-09 09:35:14 +0100

    Add support for required members

---
+++ src/PolyKit.Polyfills/System/Diagnostics/CodeAnalysis/SetsRequiredMembersAttribute.cs
@@ -0,0 +1,25 @@
+﻿#if NET7_0_OR_GREATER
+
+#if POLYKIT_PUBLIC
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Diagnostics.CodeAnalysis.SetsRequiredMembersAttribute))]
+#endif
+
+#else
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+namespace System.Diagnostics.CodeAnalysis;
+
+// https://github.com/dotnet/runtime/blob/v7.0.0/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/SetsRequiredMembersAttribute.cs
+
+/// <summary>
+/// Specifies that this constructor sets all required members for the current type, and callers
+/// do not need to set any required members themselves.
+/// </summary>
+[AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+public // polyfill!
+sealed class SetsRequiredMembersAttribute : Attribute
+{
+}
+
+#endif
---
+++ src/PolyKit.Polyfills/System/Runtime/CompilerServices/RequiredMemberAttribute.cs
@@ -0,0 +1,22 @@
+﻿#if NET7_0_OR_GREATER
+
+#if POLYKIT_PUBLIC
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Runtime.CompilerServices.RequiredMemberAttribute))]
+#endif
+
+#else
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+namespace System.Runtime.CompilerServices;
+
+// https://github.com/dotnet/runtime/blob/v7.0.0/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/RequiredMemberAttribute.cs
+
+/// <summary>Specifies that a type has required members or that a member is required.</summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+public // polyfill!
+sealed class RequiredMemberAttribute : Attribute
+{
+}
+
+#endif
